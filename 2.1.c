22fe23a31b09331c928d6940dc5cd9ba602016f4
Coinc1dens <53996313+Coinc1dens@users.noreply.github.com>
2019-09-28 19:16:53 +0800

    Delete 2.1.c

--- 2.1.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-#define PI 3.1415926535
-int main(){
-	double r;
-    printf("请输入圆或球的半径:"); 
-    scanf("%lf",&r);
-    printf("圆的周长:%.3lf\n圆的面积:%.3lf\n球的体积:%.3lf\n",2*PI*r,PI*r*r,4.0/3.0*PI*r*r*r);
-}
