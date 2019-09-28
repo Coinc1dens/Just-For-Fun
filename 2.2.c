c192040a787acd0bab79ce22fae916629bc7e3e4
Coinc1dens <53996313+Coinc1dens@users.noreply.github.com>
2019-09-28 19:17:05 +0800

    Delete 2.2.c

--- 2.2.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-int main(){
-	int a,b,c,d;
-	puts("请输入四个季度的销售额:"); 
-	scanf("%d %d %d %d",&a,&b,&c,&d);
-	printf("累计销售额如下:\n第一季度:%d\n第二季度:%d\n第三季度:%d\n第四季度:%d\n环比增长率如下:\n第二季度:%.2lf%%\n第三季度:%.2lf%%\n第四季度:%.2lf%%\n",a,a+b,a+b+c,a+b+c+d,((b-a)/(double)a)*100.0,((c-b)/(double)b)*100.0,((d-c)/(double)c)*100.0);
-}
